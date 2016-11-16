94b9fac828cfc7bc8fdf6eeecf3e443d1f082440
jamcoy <james@coynemail.net>
2016-11-16 14:26:55 +0000

    Set root url to show all posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
+    url(r'^$', views.post_list),
     url(r'^blog/$', views.post_list),
     url(r'^blogTop5/$', views.top_posts),
     url(r'^blog/(?P<db_id>\d+)/$', views.post_detail),
